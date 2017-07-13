f0a257ded34d5b0eb74506d29c36779e5464f676
Jeffrey Chrisope <jeffchrisope@live.com>
2017-07-12 21:08:42 -0700

    Continuing to add bits and bobs to the starter template.

---
+++ src/ssmods/ss_TEST/SUBSYSTEMNAME_MODULENAME.h
@@ -0,0 +1,15 @@
+/*
+ * SUBSYSTEMNAME_MODULENAME.h
+ *
+ *  Created on: Jul 12, 2017
+ *      Author: jeffc
+ */
+
+#ifndef SUBSYSTEMNAME_MODULENAME_H_
+#define SUBSYSTEMNAME_MODULENAME_H_
+
+
+
+
+
+#endif /* SUBSYSTEMNAME_MODULENAME_H_ */
--- src/ssmods/ss_TEST/SUBSYSTEMNAME_main.c
+++ src/ssmods/ss_TEST/SUBSYSTEMNAME_main.c
@@ -1,5 +1,6 @@
 #include <msp430.h> 
 
+#include "SUBSYSTEMNAME_MODULENAME.h"
 #include "bsp/bsp.h"
 
 /*
@@ -9,6 +10,12 @@ int main(void) {
 
     // ALWAYS START main() with bspInit(<systemname>) as the FIRST line of code
     bspInit(Module_Test);
+
+#if defined(__DEBUG__)
+
+    // TODO:  Insert debug-specific stuff here, including registering info/status/action handlers, etc.
+
+#endif  //  __DEBUG__
 	
 	return 0;
 }
