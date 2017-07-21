77060ffe6a7be3fa576dbf0a4298dd1aa5354a01
Unknown <emorye@me.com>
2017-07-20 17:03:30 -0700

    CAN API Branch
    Created branch for CAN API testing as well as an empty project to test with. CAN API changes coming soon. (Also updated .gitignore to ignore unnecessary Mac filesystem files.)

---
+++ src/devmods/cantest_1/SUBSYSTEMNAME_main.c
@@ -0,0 +1,23 @@
+#include <msp430.h> 
+
+#include "SUBSYSTEMNAME_MODULENAME.h"
+#include "bsp/bsp.h"
+
+/*
+ * main.c
+ */
+int main(void) {
+
+    // ALWAYS START main() with bspInit(<systemname>) as the FIRST line of code
+    bspInit(Module_Test);
+
+
+
+#if defined(__DEBUG__)
+
+    // TODO:  Insert debug-specific stuff here, including registering info/status/action handlers, etc.
+
+#endif  //  __DEBUG__
+	
+	return 0;
+}
