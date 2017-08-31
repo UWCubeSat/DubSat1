f00488629035dae94d911b37e53af1661a984529
Aaron Adler <aadler@uw.edu>
2017-08-31 10:27:59 -0700

    Git got confused

---
+++ src/dsbase/sensors/BMI160_IMU.h
@@ -0,0 +1,18 @@
+/*
+ * BMI160_IMU.h
+ *
+ *  Created on: Jul 11, 2017
+ *      Author: jeffc
+ */
+
+#ifndef SENSORS_BMI160_IMU_H_
+#define SENSORS_BMI160_IMU_H_
+
+
+// TODO:  Minimal definitions to get IMU off the ground, for assessment/characterization
+#define IMU_BMI160_I2C_7BIT_ADDRESS                 0x68
+#define IMU_BMI160_FULL_OUTPUT_REG_ADDR_START
+#define IMU_BMI160_GYRO_OUTPUT_REG_ADDR_START
+#define IMU_BMI160_ACCEL_OUTPUT_REG_ADDR_START
+
+#endif /* SENSORS_BMI160_IMU_H_ */
