ba12c97212ee18685e562aee147910bb7051b0c6
Jeffrey Chrisope <jeffchrisope@live.com>
2017-09-12 19:17:03 -0700

    Sensor characterization (pt 2):  Rest of script work for MATLAB IMU characterization.

--- teams/cdh/BusExperiments/AardvarkExperiments.m
+++ teams/cdh/BusExperiments/AardvarkExperiments.m
@@ -1,59 +1,30 @@
+clear all; close all; clc;
 
-[notfound warnings] = loadlibrary('aardvark', 'aardvark.h', 'mfilename', 'aaproto')
-unloadlibrary aardvark
-[notfound warnings] = loadlibrary('aardvark', 'aardvark.h', 'mfilename', 'aaproto')
-libfunctionsview aardvark
-calllib(lib, 'c_aa_find_devices_ext', 16, pPorts, 16, pUnique_ids)
-pPorts
-pPorts.Value
-ports
-unique_ids
-calllib(lib, 'c_aa_find_devices_ext', 16, pPorts, 16, pUnique_ids)
-libfunctionsview aardvark
-ports
-pPorts = libpointer('uint16Ptr', ports)
-pPorts.Value
-unique_ids
-pUnique_ids = libpointer('uint32Ptr', unique_ids)
-pUnique_ids.Value
-calllib(lib, 'c_aa_find_devices_ext', 16, pPorts, 16, pUnique_ids)
-ports
-unique_ids
-[outnum outports outuids] = callib(lib, 'c_aa_find_devices_ext', 16, pPorts, 16, pUnique_ids)
-[outnum outports outuids] = calllib(lib, 'c_aa_find_devices_ext', 16, pPorts, 16, pUnique_ids)
-outuids
-outports
-outnum
-int32(unique_ids)
-unique_ids = int32(unique_ids)
-ports - int16(ports)
-ports = int16(ports)
-pPorts = libpointer('uint16Ptr', ports)
-pUnique_ids = libpointer('uint32Ptr', unique_ids)
-[outnum outports outuids] = calllib(lib, 'c_aa_find_devices_ext', 16, pPorts, 16, pUnique_ids)
-outuids
-outports
-outids
-outuids
-calllib(lib, 'c_aa_open', 0)
-h = 1
-calllib(lib, 'c_aa_open', 0)
-calllib(lib, 'c_aa_close', 1)
-h = calllib(lib, 'c_aa_open', 0)
-h
-str = hex2dec('deadbeef');
-str
-outstr = [222 173 190 239]
-outstr = uint8(outstr)
-outstr
-calllib(lib, 'c_aa_i2c_slave_set_response', h, 4, outstr)
-calllib(lib, 'c_aa_i2c_slave_enable', h, 5, 4, 4)
-calllib(lib, 'c_aa_close', 1)
-h = calllib(lib, 'c_aa_open', 0)
-calllib(lib, 'c_aa_i2c_slave_set_response', h, 4, outstr)
-calllib(lib, 'c_aa_i2c_slave_enable', h, 5, 4, 4)
-outstr
-outstr(2) = 193
-calllib(lib, 'c_aa_i2c_slave_set_response', h, 4, outstr)
-outstr(2) = 173
-calllib(lib, 'c_aa_i2c_slave_set_response', h, 4, outstr)
+lib = 'aardvark';
+libhdr = 'aardvark.h';
+
+if ~libisloaded(lib)
+    [load_notfounderrors load_warnings] = loadlibrary(lib, libhdr);
+end
+
+hport = 0;
+hdev = calllib(lib, 'c_aa_open', hport);
+if (hdev < 0)
+    error('Unable to open port 0.');
+end
+
+%% Slave Setup
+% Configures Aardvark to listen as a slave, using native API wrapper
+slaveaddr = 9;
+outstr = [222 173 190 239];
+outstr = uint8(outstr);
+calllib(lib, 'c_aa_i2c_slave_set_response', hdev, length(outstr), outstr);
+calllib(lib, 'c_aa_i2c_slave_enable', hdev, slaveaddr, 0, 0);
+
+disp('Device set to slave.  Hit any key to continue ...');
+pause();
+
+calllib(lib, 'c_aa_close', hport);
+
+
+
