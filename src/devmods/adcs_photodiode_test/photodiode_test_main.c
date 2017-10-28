ae1ea09925cf386c40648b31b0afb71392c5ee67
Jerrold <jerrolde@uw.edu>
2017-10-28 14:15:08 -0700

    photodiode test project now compiles

--- src/devmods/adcs_photodiode_test/photodiode_test_main.c
+++ src/devmods/adcs_photodiode_test/photodiode_test_main.c
@@ -1,13 +1,14 @@
 #include <msp430.h>
 #include <stdint.h>
-#include "../../sensors/photodiode.h"
+#include "sensors/photodiode.h"
+#include "bsp/bsp.h"
 
 #define I2C_PORTSEL0        P7SEL0
 #define I2C_PORTSEL1        P7SEL1
 #define I2C_SDA_BIT         BIT0
 #define I2C_SCL_BIT         BIT1
 #define LED_PORT            P1OUT
-#define LED_PORT_DIR        P1DIR
+//#define LED_PORT_DIR        P1DIR
 #define LED_PIN             BIT0
 
 //both addr lines wired to vcc
@@ -15,6 +16,7 @@ const uint8_t LTC2481_I2C_ADDR = 0x26;
 
 int main(void)
 {
+    bspInit(Module_Test);
     WDTCTL = WDTPW | WDTHOLD;
 
     // Configure GPIO for status LED
@@ -39,7 +41,7 @@ int main(void)
         volatile unsigned int i;
 
         adcTemp = ltc2481Temperature();
-		
+
         i = 50000;
         while (i > 0)
         {
