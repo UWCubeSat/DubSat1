de2b6cf63b60fa1a5e02eeebf8d0297364608d45
mkyl <me@kayali.io>
2017-11-07 15:24:18 -0800

    Store configuration files in user's configuration file directory. #76

--- teams/cdh/VISAExperiments/instrument_hub/instrument_hub/configuration_parser.py
+++ teams/cdh/VISAExperiments/instrument_hub/instrument_hub/configuration_parser.py
@@ -1,36 +1,38 @@
 import toml
-import os
+from os import listdir
 from pathlib import Path
 from appdirs import user_config_dir
-
-INSTRUMENT_CONFIGURATION_FOLDER = "instrument-configurations"
 
 class ConfigurationParser:
 	def get_instrument_configurations():
 		instruments = []
 
-		print(__name__)
-		assert False
-		#configuration_folder = Path(user_config_dir(__name__))
-		assert configuration_folder.is_dir()
-		print(os.fspath(configuration_folder))
+		# configuration folder is located in <user config file path>/instrument_hub/
+		configuration_folder = Path(user_config_dir(__package__))
+		if not configuration_folder.exists():
+			configuration_folder.mkdir()
 
-		for instrument_config in os.listdir(configuration_folder):
-			print(instrument_config)
-			#if not instrument_config.is_dir():
-			with open(instrument_config, 'r') as f:
-				print(f.read())
-				parsed_config = toml.loads(f.read())
-				validate_configuration(parsed_config)
-				instruments.append(parsed_config)
+		for config_file_name in listdir(configuration_folder):
+			config_file = Path(configuration_folder, config_file_name)
+			if not config_file.is_dir():
+				with open(config_file, 'r') as f:
+					parsed_config = toml.loads(f.read())
+					ConfigurationParser.validate_configuration(parsed_config)
+					instruments.append(parsed_config)
+
+		if len(instruments) == 1:
+			print("[i] Parsed", len(instruments), "instrument configuration file.")
+		else:
+			print("[i] Parsed", len(instruments), "instrument configuration files.")
 
 		return instruments
 
 	def validate_configuration(config_toml):
-		is_valid = true
-		is_valid &= config_toml["instrument-information"]
-		is_valid &= config_toml["instrument-information"].friendly-name
-		is_valid &= config_toml["instrument-information"].visa-name
-
-		if not valid:
+		# if any of these keys are not in the config file, KeyError is fired
+		try:
+			config_toml["instrument-information"]
+			config_toml["instrument-information"]["visa-make"]
+			config_toml["instrument-information"]["visa-model"]
+			config_toml["instrument-information"]["port"]
+		except KeyError:
 			raise ValueError("Invalid configuration file.")
