489bc0e6fc489c9eb44ca880fee282827fdfa652
Emory Eng <emorye@me.com>
2018-02-11 12:58:40 -0800

    Increased bus speed to production speed

--- src/devmods/cantest_1/SUBSYSTEMNAME_main.c
+++ src/devmods/cantest_1/SUBSYSTEMNAME_main.c
@@ -18,21 +18,34 @@
 
 void newCB(CANPacket *packet){
 
-    Message4OddSizes pst = {0};
-    CANPacket ppst = {0};
-    CANPacket *pps = &ppst;
-    pst.OddSize3 = 69;
-    encodeMessage4OddSizes(&pst,pps);
-    canSendPacket(pps);
+}
+
+uint32_t testBICount;
+
+void testBIGetPacket(CANPacket *packet){
+    testBICount++;
+}
+
+void testBIInit(){
+    testBICount = 0;
+    canWrapInit();
+    setCANPacketRxCallback(testBIGetPacket);
 }
 
 int main(void) {
 
     // ALWAYS START main() with bspInit(<systemname>) as the FIRST line of code
     bspInit(Module_Test);
+    testBIInit();
 
-    canWrapInit();
-    setCANPacketRxCallback(newCB);
+    P1OUT &= 0x00;               // Shut down everything
+    P1DIR &= 0x00;
+    P1DIR |= BIT0 + BIT6;       // P1.0 and P1.6 pins output the rest are input
+    P1REN |= BIT3;                 // Enable internal pull-up/down resistors
+    P1OUT |= BIT3;                 //Select pull-up mode for P1.3
+    P1IE |= BIT3;                    // P1.3 interrupt enabled
+    P1IES |= BIT3;                  // P1.3 Hi/lo edge
+    P1IFG &= ~BIT3;               // P1.3 IFG cleared
 
     P3DIR |= 0x20;
     P3OUT |= 0x20;
@@ -47,4 +60,23 @@ int main(void) {
 #endif  //  __DEBUG__
 	
 	return 0;
+}
+
+
+// Port 1 interrupt service routine
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)
+{
+
+   CANPacket p = {0};
+   p.length = 8;
+   p.data[4] = (uint8_t) testBICount;
+   p.data[3] = (uint8_t) (testBICount >> 8);
+   p.data[2] = (uint8_t) (testBICount >> 16);
+   p.data[1] = (uint8_t) (testBICount >> 24);
+   p.data[0] = 0x69;
+   canSendPacket(&p);
+
+   testBICount = 0;
+   P1IFG &=~BIT3;                        // P1.3 IFG cleared
 }
