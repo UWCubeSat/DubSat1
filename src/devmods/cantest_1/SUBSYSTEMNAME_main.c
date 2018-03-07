8ee6e9dfafa248733cef7c05bb3dda16823d85c8
Emory Eng <emorye@me.com>
2018-03-06 16:32:44 -0800

    Implemented Filter API

--- src/devmods/cantest_1/SUBSYSTEMNAME_main.c
+++ src/devmods/cantest_1/SUBSYSTEMNAME_main.c
@@ -5,6 +5,14 @@
 #include "core/can.h"
 #include "interfaces/canwrap.h"
 
+
+// Directives for timer stuff.
+#include "core/timers.h"
+#define FREQ_TIMER(reg)         TA4##reg
+#define FREQ_ROOT_TIMER(bits)   TA##bits
+FILE_STATIC uint32_t overflowCount = 0;
+
+
 #define BOARD_NUM (0x05)
 
 #define LED_BLUE (0x01)
@@ -15,9 +23,20 @@
  * main.c
  */
 // Send back the same reply
+void blinkCallback(CANPacket *p){
+    if(p->bufferNum){
+        PJDIR |= 0x02;
+        PJOUT ^= 0x02;
+    }
+    else{
+        PJDIR |= 0x01;
+        PJOUT ^= 0x01;
+    }
 
-void newCB(CANPacket *packet){
-
+}
+void testFilterCB(CANPacket *packet){
+    PJDIR |= 0x02;
+    PJOUT ^= 0x02;
 }
 
 uint32_t testBICount;
@@ -30,13 +49,6 @@ void testBIInit(){
     testBICount = 0;
     canWrapInit();
     setCANPacketRxCallback(testBIGetPacket);
-}
-
-int main(void) {
-
-    // ALWAYS START main() with bspInit(<systemname>) as the FIRST line of code
-    bspInit(Module_Test);
-    testBIInit();
 
     P1OUT &= 0x00;               // Shut down everything
     P1DIR &= 0x00;
@@ -47,9 +59,36 @@ int main(void) {
     P1IES |= BIT3;                  // P1.3 Hi/lo edge
     P1IFG &= ~BIT3;               // P1.3 IFG cleared
 
-    P3DIR |= 0x20;
-    P3OUT |= 0x20;
+}
 
+void testTimerInit(){
+    canWrapInit();
+    // Init the timer
+    FREQ_TIMER(CCTL0) = CM__RISING | CCIS__CCIB | SCS | CAP | CCIE;
+    FREQ_TIMER(CTL) = FREQ_ROOT_TIMER(SSEL__ACLK) | MC__CONTINUOUS | FREQ_ROOT_TIMER(CLR) | TAIE;
+
+
+}
+
+void testFilter(){
+    canWrapInitWithFilter();
+    setCANPacketRxCallback(blinkCallback);
+//    FREQ_TIMER(CCTL0) = CM__RISING | CCIS__CCIB | SCS | CAP | CCIE;
+//    FREQ_TIMER(CTL) = FREQ_ROOT_TIMER(SSEL__ACLK) | MC__CONTINUOUS | FREQ_ROOT_TIMER(CLR) | TAIE;
+}
+int main(void) {
+
+    // ALWAYS START main() with bspInit(<systemname>) as the FIRST line of code
+    bspInit(Module_Test);
+//    testBIInit();
+//    testTimerInit();
+//    canWrapInit();
+     testFilter();
+    while(1){
+//        CANPacket p = {0};
+//        p.length = 7;
+//        canSendPacket(&p);
+    }
 
 
 
@@ -61,22 +100,46 @@ int main(void) {
 	
 	return 0;
 }
+void testBIPoll(){
+    CANPacket p = {0};
+    p.length = 8;
+    p.data[4] = (uint8_t) testBICount;
+    p.data[3] = (uint8_t) (testBICount >> 8);
+    p.data[2] = (uint8_t) (testBICount >> 16);
+    p.data[1] = (uint8_t) (testBICount >> 24);
+    p.data[0] = 0x69;
+    canSendPacket(&p);
 
+    testBICount = 0;
+    P1IFG &=~BIT3;                        // P1.3 IFG cleared
+}
 
 // Port 1 interrupt service routine
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void)
 {
+    // testBIPoll();
+}
 
-   CANPacket p = {0};
-   p.length = 8;
-   p.data[4] = (uint8_t) testBICount;
-   p.data[3] = (uint8_t) (testBICount >> 8);
-   p.data[2] = (uint8_t) (testBICount >> 16);
-   p.data[1] = (uint8_t) (testBICount >> 24);
-   p.data[0] = 0x69;
-   canSendPacket(&p);
-
-   testBICount = 0;
-   P1IFG &=~BIT3;                        // P1.3 IFG cleared
+// Timer4_A1 (CCR=1..n) TA Interrupt Handler
+#pragma vector = TIMER4_A1_VECTOR
+__interrupt void Timer4_A1_ISR(void)
+{
+    switch (__even_in_range(TA4IV, TAIV__TAIFG))
+    {
+        case TAIV__TAIFG:                   // TA4.0 overflow
+            // Deal with bogus initial overflow flag that pops on startup
+            PJDIR |= 0x01;
+            PJOUT ^= 0x01;
+            overflowCount++;
+            // ONCE EVERY TWO SECONDS
+            CANPacket p = {0};
+            p.id = 0xBADA55;
+            p.data[0] = overflowCount;
+            p.length = 1;
+            canSendPacket(&p);
+            break;
+        default:
+            break;
+    }
 }
