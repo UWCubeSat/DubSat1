e6a4cf6461be050cb89586bd5979ed3d21e5cb36
Sean poulter <spoulter@uw.edu>
2018-03-13 00:11:23 -0700

    EPS code: Fixed conversion errors in coulomb counter driver

--- src/dsbase/sensors/coulomb_counter.c
+++ src/dsbase/sensors/coulomb_counter.c
@@ -19,20 +19,25 @@ void LTC2943Init(bus_instance_i2c bus, float rShunt) {
     i2cBuff[1] = ALERTDISABLED_PRESCALER256_ADCAUTOMATICMODE;
     i2cMasterWrite(hSensor, i2cBuff, 2);
 
+//    //read set control register
+//    i2cMasterRegisterRead(sensor.hI2CDevice, LTC2943_ADDR_CONTROL, i2cBuff, 1);
+//    sensor.controlReg = i2cBuff[0];
+
     //calculate charge LSB based on shunt resistance and prescaler.
-    sensor.chargeLSB = 0.34*(50/sensor.shuntResistance)*(LTC2943_PRESCALEFACTOR/4096.0);
+    sensor.chargeLSB = 0.34*(50.0/sensor.shuntResistance)*((float)LTC2943_PRESCALEFACTOR/4096.0);
 
     //calculate accumulated charge at full/empty battery. Assuming 2200mAh battery
-    sensor.accumChargeFull = 0x7FFF+((2200/sensor.chargeLSB)/2);
-    sensor.accumChargeEmpty = 0x7FFF-((2200/sensor.chargeLSB)/2);
+    sensor.accumChargeFull = 0x7FFF+((2200.0/sensor.chargeLSB)/2);
+    sensor.accumChargeEmpty = 0x7FFF-((2200.0/sensor.chargeLSB)/2);
+
 }
 
 float rawBusVoltageToFloat(int16_t raw) {
-    return 23.6*(raw/0xFFFF);
+    return 23.6*((float)raw/(float)0xFFFF);
 }
 
 float rawCurrentToFloat(int16_t raw) {
-    return ((60.0/sensor.shuntResistance)*((raw-0x7FFF)/0x7FFF));
+    return ((60.0/sensor.shuntResistance)*((float)(raw-0x7FFF)/(float)0x7FFF));
 }
 
 BOOL checkForFullState(float voltage, float current) {
@@ -56,17 +61,17 @@ void calibrate(hDev hSensor) {
 CoulombCounterData readCoulombCounter() {
     //read voltage registers with repeated start condition.
     i2cMasterRegisterRead(sensor.hI2CDevice, LTC2943_ADDR_VOLTAGE_MSB, i2cBuff, 2);
-    sensor.sensorData.rawBusVoltage = (int16_t)(i2cBuff[1] | ((int16_t)i2cBuff[0]<<8));
+    sensor.sensorData.rawBusVoltage = (uint16_t)(i2cBuff[1] | ((uint16_t)i2cBuff[0]<<8));
     sensor.sensorData.busVoltageV = rawBusVoltageToFloat(sensor.sensorData.rawBusVoltage);
 
     //read Current registers with repeated start condition
     i2cMasterRegisterRead(sensor.hI2CDevice, LTC2943_ADDR_CURRENT_MSB, i2cBuff, 2);
-    sensor.sensorData.rawCurrent = (int16_t)(i2cBuff[1] | ((int16_t)i2cBuff[0]<<8));
+    sensor.sensorData.rawCurrent = (uint16_t)(i2cBuff[1] | ((uint16_t)i2cBuff[0]<<8));
     sensor.sensorData.calcdCurrentA = rawCurrentToFloat(sensor.sensorData.rawCurrent);
 
     //read Charge registers with repeated start condition
     i2cMasterRegisterRead(sensor.hI2CDevice, LTC2943_ADDR_ACCUMULATED_CHARGE_MSB, i2cBuff, 2);
-    sensor.sensorData.rawAccumCharge = (int16_t)(i2cBuff[1] | ((int16_t)i2cBuff[0]<<8));
+    sensor.sensorData.rawAccumCharge = ((uint16_t)i2cBuff[1] | ((uint16_t)i2cBuff[0]<<8));
 
     //if the batteries are full, then calibrate the mAh measurement.
     if(checkForFullState(sensor.sensorData.busVoltageV, sensor.sensorData.calcdCurrentA)){
@@ -74,10 +79,10 @@ CoulombCounterData readCoulombCounter() {
     }
 
     //State of charge calculation
-    sensor.sensorData.SOC = sensor.chargeLSB*((sensor.sensorData.rawAccumCharge - sensor.accumChargeEmpty)/(sensor.accumChargeFull - sensor.accumChargeEmpty));
+    sensor.sensorData.SOC = sensor.chargeLSB*((float)(sensor.sensorData.rawAccumCharge - sensor.accumChargeEmpty)/(float)(sensor.accumChargeFull - sensor.accumChargeEmpty));
 
     //Accumulated charge calculation. Relative to the entire register I.E it is calculating the [mAh worth of 1 bit change] times the [integer value of the register].
-    sensor.sensorData.accumulatedCharge = sensor.chargeLSB*sensor.sensorData.rawAccumCharge;
+    sensor.sensorData.totalAccumulatedCharge = sensor.chargeLSB*sensor.sensorData.rawAccumCharge;
 
 
     //Accumulated charge in batteries, assuming 2200mAh batteries.
