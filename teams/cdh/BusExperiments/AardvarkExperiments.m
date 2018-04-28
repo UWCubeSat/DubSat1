7d471e368ac677da59c05751a39f4969b5070517
DESKTOP-HEUSEBO\SatLab <jeffchrisope@live.com>
2018-04-27 21:12:05 -0700

    Test inputs to suss out issues with Aardvark

--- teams/cdh/BusExperiments/AardvarkExperiments.m
+++ teams/cdh/BusExperiments/AardvarkExperiments.m
@@ -1,4 +1,4 @@
- clear all; close all; clc;
+                                                                                                                                        clear all; close all; clc;
 
 lib = 'aardvark';
 libhdr = 'aardvark.h';
@@ -27,20 +27,37 @@ slaveaddr = hex2dec('1E');
 % 
 % calllib(lib, 'c_aa_close', hport);
 
-outstr = [0 0 0 0 0 0];
+outstr = [0 50 100 150 200 0];
 outstr = uint8(outstr);
 
 calllib(lib, 'c_aa_i2c_slave_set_response', hdev, length(outstr), outstr);
 calllib(lib, 'c_aa_i2c_slave_enable', hdev, slaveaddr, 0, 0);
 
+t = 0:.02:1000;
+scalefactor = 127;
+outx = floor(sin(t) * scalefactor);
+outy = floor(sin(t+0.5*pi) * scalefactor);
+outz = floor(sin(t+pi) * scalefactor);
+
+i = 0;
+count = 0;
 while 1
-   newval = outstr(6) + 1;
-   if (newval == 255)
-       newval = 0;
+%    outstr = outstr + uint8([1 2 2 3 3 4]);
+%    rollover = (outstr >= 255);
+%    outstr(rollover) = 0;
+   i = i + 1;
+   if (i >=length(t))
+       i = 1;
    end
-   outstr(6) = newval;
+   outstr = int8([outx(i) 0 outz(i) 0 outy(i) 0]);
    calllib(lib, 'c_aa_i2c_slave_set_response', hdev, length(outstr), outstr);
-   newval
+   pause(.1)
+   if (mod(count,4)==0)
+       calllib(lib, 'c_aa_i2c_free_bus', hdev);
+   end
+   count = count + 1;
+   count
+   outstr
 end
 
-
+           
