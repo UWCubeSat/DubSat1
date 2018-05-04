09f5575879d4342fafe58a49bccdae172fc7889e
David Dupre <djdupre@uw.edu>
2018-05-03 17:48:07 -0700

    ADCS/Sensor proc.: Add sendSun script

--- teams/cdh/BusExperiments/ADCS_SP_Experiment/sendSun.m
+++ teams/cdh/BusExperiments/ADCS_SP_Experiment/sendSun.m
@@ -11,16 +11,9 @@ end
 
 disp('loading .dat files');
 load(strcat(dataDir, 'time.dat'));
-load(sprintf('%sin_imux_msb.dat', dataDir));
-load(sprintf('%sin_imux_lsb.dat', dataDir));
-load(sprintf('%sin_imuy_msb.dat', dataDir));
-load(sprintf('%sin_imuy_lsb.dat', dataDir));
-load(sprintf('%sin_imuz_msb.dat', dataDir));
-load(sprintf('%sin_imuz_lsb.dat', dataDir));
-load(sprintf('%sin_imuvalid.dat', dataDir));
-
-outstr = [in_imux_msb in_imux_lsb in_imuy_msb in_imuy_lsb in_imuz_msb in_imuz_lsb];
-outstr = uint8(outstr);
+load(sprintf('%sin_sunalpha.dat', dataDir));
+load(sprintf('%sin_sunbeta.dat', dataDir));
+load(sprintf('%sin_sunvalid.dat', dataDir));
 
 disp('doing slave stuff');
 
@@ -32,16 +25,24 @@ end
 
 %% Slave Setup
 % Configures Aardvark to listen as a slave, using native API wrapper
-slaveaddr = hex2dec('6A');
+slaveaddr = hex2dec('60');
 calllib(lib, 'c_aa_i2c_slave_enable', hdev, slaveaddr, 0, 0);
 
 % Sending response
 while 1
     tic
     index = 1;
-    while index <= length(outstr)
+    while index <= length(in_sunalpha)
         if (toc >= time(index + 1))
-            thisout = outstr(index, :);
+            alphaBytes = typecast(single(in_sunalpha(index)), 'uint8');
+            betaBytes = typecast(single(in_sunbeta(index)), 'uint8');
+            if in_sunvalid(index)
+                errorCode = 3; % dark
+            else
+                errorCode = 0; % OK
+            end
+            errorCode = typecast(errorCode, 'uint8');
+            thisout = [ 10 alphaBytes betaBytes errorCode ];
             calllib(lib, 'c_aa_i2c_slave_set_response', hdev, length(thisout), thisout);
             index = index + 1;
         end
