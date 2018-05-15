35cdf9892c72a522f16d1b8b118ee50c708cc9ed
David Dupre <djdupre@uw.edu>
2018-05-15 10:22:49 -0700

    Make sensors script less annoying to use
    Loads faster and automatically determines enabled/disabled state based on which aardvarks are plugged in.

--- teams/cdh/BusExperiments/ADCS_sensors/sendData.m
+++ teams/cdh/BusExperiments/ADCS_sensors/sendData.m
@@ -1,144 +1,36 @@
 clear all; close all; clc;
 
 MAX_NUM_AARDVARKS = 10;
-PORT_ERROR_VALUE = uint16(10000);
-
-dataDir = 'C:\dubsat_data\';
-spDataDir = [dataDir, 'sp\'];
-bdotDataDir = [dataDir, 'bdot\'];
-load(strcat(spDataDir, 'time.dat')); % loading from sensor proc
-dataLength = length(time);
 
 % define sensors
 
-imu.msp = 'sp';
 imu.name = 'imu';
 imu.addr = hex2dec('6A');
-imu.enabled = true;
-imu.id = 2238592316;      % unique ID to match up with aardvark
-imu.port = PORT_ERROR_VALUE;
-imu.bytes = zeros(dataLength, 10);
-imu.length = 6;
+imu.id = 2238592316; % unique ID to match up with aardvark
 
-mag1.msp = 'sp';
 mag1.name = 'mag1';
 mag1.addr = hex2dec('1E');
-mag1.enabled = false;
 mag1.id = 2238593289;
-mag1.port = PORT_ERROR_VALUE;
-mag1.bytes = zeros(dataLength, 10);
-mag1.length = 6;
 
-mag2.msp = 'sp';
 mag2.name = 'mag2';
 mag2.addr = hex2dec('1E');
-mag2.enabled = false;
 mag2.id = 0;
-mag2.port = PORT_ERROR_VALUE;
-mag2.bytes = zeros(dataLength, 10);
-mag2.length = 6;
 
-sun.msp = 'sp';
 sun.name = 'sun';
 sun.addr = hex2dec('60');
-sun.enabled = false;
 sun.id = 0;
-sun.port = PORT_ERROR_VALUE;
-sun.bytes = zeros(dataLength, 10);
-sun.length = 10;
 
-mag.msp = 'bdot';
-mag.name = 'mag';
+mag.name = 'bdot-mag';
 mag.addr = hex2dec('1E');
-mag.enabled = false;
 mag.id = 2238519142;
-mag.port = PORT_ERROR_VALUE;
-mag.bytes = zeros(dataLength, 10);
-mag.length = 6;
 
-% load data
-disp('loading data');
-if (imu.enabled)
-    load(sprintf('%sin_imux_msb.dat', spDataDir));
-    load(sprintf('%sin_imux_lsb.dat', spDataDir));
-    load(sprintf('%sin_imuy_msb.dat', spDataDir));
-    load(sprintf('%sin_imuy_lsb.dat', spDataDir));
-    load(sprintf('%sin_imuz_msb.dat', spDataDir));
-    load(sprintf('%sin_imuz_lsb.dat', spDataDir));
-    load(sprintf('%sin_imuvalid.dat', spDataDir));
-    imu.bytes = [in_imux_msb in_imux_lsb in_imuy_msb in_imuy_lsb in_imuz_msb in_imuz_lsb];
-    imu.bytes = uint8(imu.bytes);
-end
-if (mag1.enabled)
-    load(sprintf('%sin_mag1x_msb.dat', spDataDir));
-    load(sprintf('%sin_mag1x_lsb.dat', spDataDir));
-    load(sprintf('%sin_mag1y_msb.dat', spDataDir));
-    load(sprintf('%sin_mag1y_lsb.dat', spDataDir));
-    load(sprintf('%sin_mag1z_msb.dat', spDataDir));
-    load(sprintf('%sin_mag1z_lsb.dat', spDataDir));
-    load(sprintf('%sin_mag1valid.dat', spDataDir));
-    mag1.bytes = [in_mag1x_msb in_mag1x_lsb in_mag1z_msb in_mag1z_lsb in_mag1y_msb in_mag1y_lsb];
-    mag1.bytes = uint8(mag1.bytes);
-end
-if (mag2.enabled)
-    load(sprintf('%sin_mag2x_msb.dat', spDataDir));
-    load(sprintf('%sin_mag2x_lsb.dat', spDataDir));
-    load(sprintf('%sin_mag2y_msb.dat', spDataDir));
-    load(sprintf('%sin_mag2y_lsb.dat', spDataDir));
-    load(sprintf('%sin_mag2z_msb.dat', spDataDir));
-    load(sprintf('%sin_mag2z_lsb.dat', spDataDir));
-    load(sprintf('%sin_mag2valid.dat', spDataDir));
-    mag2.bytes = [in_mag2x_msb in_mag2x_lsb in_mag2z_msb in_mag2z_lsb in_mag2y_msb in_mag2y_lsb];
-    mag2.bytes = uint8(mag2.bytes);
-end
-if (sun.enabled)
-    load(sprintf('%sin_sunalpha.dat', spDataDir));
-    load(sprintf('%sin_sunbeta.dat', spDataDir));
-    load(sprintf('%sin_sunvalid.dat', spDataDir));
-    sunLength = length(in_sunalpha);
-    alphaBytes = zeros(1, sunLength);
-    betaBytes = zeros(1, sunLength);
-    errorCodes = zeros(1, sunLength);
-    sun.bytes = zeros(1, sunLength);
-    for i=1:length(alphaBytes)
-        alphaBytes(i) = typecast(single(in_sunalpha(index)), 'uint8');
-        betaBytes(i) = typecast(single(in_sunbeta(index)), 'uint8');
-        if in_sunvalid(index)
-            errorCode = 3; % dark
-        else
-            errorCode = 0; % OK
-        end
-        errorCodes(i) = typecast(errorCode, 'uint8');
-        sun.bytes(i) = [ 10 alphaBytes betaBytes errorCode ];
-    end
-end
-if (mag.enabled)
-    xMagMsb = uint8(dlmread(strcat(bdotDataDir, 'xMagMsb_to.dat')));
-    yMagMsb = uint8(dlmread(strcat(bdotDataDir, 'yMagMsb_to.dat')));
-    zMagMsb = uint8(dlmread(strcat(bdotDataDir, 'zMagMsb_to.dat')));
-    xMagLsb = uint8(dlmread(strcat(bdotDataDir, 'xMagLsb_to.dat')));
-    yMagLsb = uint8(dlmread(strcat(bdotDataDir, 'yMagLsb_to.dat')));
-    zMagLsb = uint8(dlmread(strcat(bdotDataDir, 'zMagLsb_to.dat')));
-    mag.bytes = [xMagMsb xMagLsb zMagMsb zMagLsb yMagMsb yMagLsb];
-    mag.bytes = uint8(mag.bytes);
-end
-
-sensors = [];
-if (imu.enabled)
-    sensors = [sensors, imu];
-end
-if (mag1.enabled)
-    sensors = [sensors, mag1];
-end
-if (mag2.enabled)
-    sensors = [sensors, mag2];
-end
-if (sun.enabled)
-    sensors = [sensors, sun];
-end
-if (mag.enabled)
-    sensors = [sensors, mag];
-end
+sensors = [
+    imu
+    mag1
+    mag2
+    sun
+    mag
+];
 
 % load aardvark library
 disp('loading library');
@@ -149,7 +41,7 @@ if ~libisloaded(lib)
 end
 
 % find aardvarks
-disp('initializing aardvarks');
+disp('assigning sensors to ports');
 portsPtr = libpointer('uint16Ptr', uint16(zeros(1, MAX_NUM_AARDVARKS)));
 idsPtr = libpointer('uint32Ptr', uint32(zeros(1, MAX_NUM_AARDVARKS)));
 numAardvarks = calllib(lib, 'c_aa_find_devices_ext', MAX_NUM_AARDVARKS, portsPtr, MAX_NUM_AARDVARKS, idsPtr);
@@ -157,19 +49,95 @@ ports = get(portsPtr, 'Value');
 ids = get(idsPtr, 'Value');
 
 % assign ports to each sensor
-for i=1:length(sensors)
+i = 1;
+while i <= length(sensors)
     % find an aardvark for this sensor
+    foundAardvark = false;
     for j=1:numAardvarks
         if ids(j) == sensors(i).id
             sensors(i).port = ports(j);
+            fprintf('  assigned port %i to %s\n', sensors(i).port, sensors(i).name);
+            foundAardvark = true;
+            i = i + 1;
+            break
         end
     end
-    if sensors(i).port == PORT_ERROR_VALUE
-        error('no aardvark found for %s!', sensors(i).name);
+    if ~foundAardvark
+        % remove sensors that have no aardvarks
+        disp(['  no aardvark found for ', sensors(i).name]);
+        sensors(i) = [];
+    end
+end
+if isempty(sensors)
+    error('no matching aardvarks found!');
+end
+
+% load data
+disp('loading data');
+tic
+dataDir = 'C:\dubsat_data\';
+spDataDir = [dataDir, 'sp\'];
+bdotDataDir = [dataDir, 'bdot\'];
+time = dlmread(strcat(spDataDir, 'time.dat'));
+for i=1:length(sensors)
+    % load sensors based on their id (but only the ones with an aardvark)
+    switch sensors(i).id
+        case imu.id
+            in_imux_msb = dlmread(sprintf('%sin_imux_msb.dat', spDataDir));
+            in_imux_lsb = dlmread(sprintf('%sin_imux_lsb.dat', spDataDir));
+            in_imuy_msb = dlmread(sprintf('%sin_imuy_msb.dat', spDataDir));
+            in_imuy_lsb = dlmread(sprintf('%sin_imuy_lsb.dat', spDataDir));
+            in_imuz_msb = dlmread(sprintf('%sin_imuz_msb.dat', spDataDir));
+            in_imuz_lsb = dlmread(sprintf('%sin_imuz_lsb.dat', spDataDir));
+            sensors(i).bytes = [in_imux_msb in_imux_lsb in_imuy_msb in_imuy_lsb in_imuz_msb in_imuz_lsb];
+        case mag1.id
+            in_mag1x_msb = dlmread(sprintf('%sin_mag1x_msb.dat', spDataDir));
+            in_mag1x_lsb = dlmread(sprintf('%sin_mag1x_lsb.dat', spDataDir));
+            in_mag1y_msb = dlmread(sprintf('%sin_mag1y_msb.dat', spDataDir));
+            in_mag1y_lsb = dlmread(sprintf('%sin_mag1y_lsb.dat', spDataDir));
+            in_mag1z_msb = dlmread(sprintf('%sin_mag1z_msb.dat', spDataDir));
+            in_mag1z_lsb = dlmread(sprintf('%sin_mag1z_lsb.dat', spDataDir));
+            sensors(i).bytes = [in_mag1x_msb in_mag1x_lsb in_mag1z_msb in_mag1z_lsb in_mag1y_msb in_mag1y_lsb];
+        case mag2.id
+            in_mag2x_msb = dlmread(sprintf('%sin_mag2x_msb.dat', spDataDir));
+            in_mag2x_lsb = dlmread(sprintf('%sin_mag2x_lsb.dat', spDataDir));
+            in_mag2y_msb = dlmread(sprintf('%sin_mag2y_msb.dat', spDataDir));
+            in_mag2y_lsb = dlmread(sprintf('%sin_mag2y_lsb.dat', spDataDir));
+            in_mag2z_msb = dlmread(sprintf('%sin_mag2z_msb.dat', spDataDir));
+            in_mag2z_lsb = dlmread(sprintf('%sin_mag2z_lsb.dat', spDataDir));
+            sensors(i).bytes = [in_mag2x_msb in_mag2x_lsb in_mag2z_msb in_mag2z_lsb in_mag2y_msb in_mag2y_lsb];
+        case sun.id
+            in_sunalpha = dlmread(sprintf('%sin_sunalpha.dat', spDataDir));
+            in_sunbeta = dlmread(sprintf('%sin_sunbeta.dat', spDataDir));
+            in_sunvalid = dlmread(sprintf('%sin_sunvalid.dat', spDataDir));
+            sunLength = length(in_sunalpha);
+            sensors(i).bytes = zeros(sunLength, 10);
+            for j=1:sunLength
+                alphaBytes = typecast(single(in_sunalpha(j)), 'uint8');
+                betaBytes = typecast(single(in_sunbeta(j)), 'uint8');
+                if in_sunvalid(j)
+                    errorCode = 3; % dark
+                else
+                    errorCode = 0; % OK
+                end
+                errorCode = uint8(errorCode);
+                sensors(i).bytes(j, :) = [ 10 alphaBytes betaBytes errorCode ];
+            end
+        case mag.id
+            xMagMsb = uint8(dlmread(strcat(bdotDataDir, 'xMagMsb_to.dat')));
+            yMagMsb = uint8(dlmread(strcat(bdotDataDir, 'yMagMsb_to.dat')));
+            zMagMsb = uint8(dlmread(strcat(bdotDataDir, 'zMagMsb_to.dat')));
+            xMagLsb = uint8(dlmread(strcat(bdotDataDir, 'xMagLsb_to.dat')));
+            yMagLsb = uint8(dlmread(strcat(bdotDataDir, 'yMagLsb_to.dat')));
+            zMagLsb = uint8(dlmread(strcat(bdotDataDir, 'zMagLsb_to.dat')));
+            sensors(i).bytes = uint8([xMagMsb xMagLsb zMagMsb zMagLsb yMagMsb yMagLsb]);
+        otherwise
+            error('%s with id %i not recognized!', sensors(i).name, sensors(i).id);
     end
 end
 
 % open and enable aardvarks
+disp('opening aardvarks');
 for i=1:length(sensors)
     % close then open aardvark
     calllib(lib, 'c_aa_close', sensors(i).port);
@@ -177,7 +145,6 @@ for i=1:length(sensors)
     
     % enable aardvark
     calllib(lib, 'c_aa_i2c_slave_enable', sensors(i).hdev, sensors(i).addr, 0, 0);
-    disp(['  ' sensors(i).name ' enabled']);
 end
 
 % set responses
@@ -188,12 +155,8 @@ while 1
     while index < length(time)
         if (toc >= time(index + 1))
             for i=1:length(sensors)
-                sensor = sensors(i);
-                if ~sensor.enabled
-                    continue 
-                end
-                bytes = sensor.bytes(index, :);
-                calllib(lib, 'c_aa_i2c_slave_set_response', sensor.hdev, sensor.length, bytes);
+                bytes = sensors(i).bytes(index, :);
+                calllib(lib, 'c_aa_i2c_slave_set_response', sensors(i).hdev, length(bytes), bytes);
             end
             index = index + 1;
         end
