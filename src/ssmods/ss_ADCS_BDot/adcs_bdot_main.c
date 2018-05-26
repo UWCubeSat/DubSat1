74fab7a3a16c7f05775913f93bd2bef157223fec
Thu Phan <thuphan97@gmail.com>
2018-05-25 17:40:34 -0700

    added comments

--- src/ssmods/ss_ADCS_BDot/adcs_bdot_main.c
+++ src/ssmods/ss_ADCS_BDot/adcs_bdot_main.c
@@ -9,11 +9,16 @@
 #include "interfaces/rollcall.h"
 #include "core/agglib.h"
 
+#define MAG_READING_NORM_OP 1
+#define MAG_READING_TEST_OP 0
+#define MAG_READING_CURR_OP MAG_READING_TEST_OP
+
+
 /******************COSMOS Telemetry******************************/
 FILE_STATIC health_segment hseg;
 FILE_STATIC meta_segment metaSeg;
 FILE_STATIC magnetometer_segment myTelemMagnetometer;
-FILE_STATIC mtq_info_segment myTelemMtqInfo;
+FILE_STATIC mtq_info_segment cosmos_bdot_persp_mtq_info;
 FILE_STATIC simulink_segment mySimulink;
 FILE_STATIC polling_timer_info_segment polling_timer_info;
 /****************************************************************/
@@ -21,21 +26,21 @@ FILE_STATIC polling_timer_info_segment polling_timer_info;
 
 /************ MTQ Info From Bdot Perspective*********************/
 FILE_STATIC uint8_t mtq_state = MTQ_MEASUREMENT_PHASE;
-FILE_STATIC mtq_info mtqInfo;
-FILE_STATIC mtq_info lastKnownState;
-FILE_STATIC volatile magDataStatus mag_data = mag_invalid;
+FILE_STATIC mtq_info bdot_perspective_mtq_info;
+FILE_STATIC mtq_info mtq_last_known_state;
 /****************************************************************/
 
 
 /****************Magnetometer Variables*************************/
-FILE_STATIC MagnetometerData* magData;
-FILE_STATIC hMag magNum;
-FILE_STATIC uint8_t magNormalOperation = 1;
+FILE_STATIC MagnetometerData* bdot_mag_data;
+FILE_STATIC MagnetometerData* proc_mag1_data;
+FILE_STATIC MagnetometerData* proc_mag2_data;
+FILE_STATIC hMag mag_num;
+FILE_STATIC uint8_t mag_norm_op = 1;
 /***************************************************************/
 
 /******************Simulink Flags and Information***************/
-FILE_STATIC uint8_t send_dipole_flag = 0;
-FILE_STATIC uint8_t update_rt_flag = 0;
+FILE_STATIC uint8_t rt_flag = 0;
 /***************************************************************/
 
 /******************Timer Information***************************/
@@ -52,10 +57,13 @@ FILE_STATIC const rollcall_fn rollcallFunctions[] =
 {
  rcPopulate1, rcPopulate2, rcPopulate3, rcPopulate4
 };
+/*******************RollCall***********************************/
+
 
 /***************************************************************/
 /*******************Agg Library***********************************/
 /***************************************************************/
+
 
 /*******************Miscellaneous*******************************/
 FILE_STATIC ModuleStatus mod_status;
@@ -95,43 +103,49 @@ int main(void)
     startCallback(rtOneStep_timer);
 
     fflush((NULL));
-    int count = 0;
+
     while (rtmGetErrorStatus(rtM) == (NULL) || 1)
     {
 
-        if(update_rt_flag)
+        if(rt_flag)
         {
             P3OUT ^= BIT5;
-            getMagnetometerData();
-            rtU.B_body_in_T[0] = magData->convertedX;
-            rtU.B_body_in_T[1] = magData->convertedY;
-            rtU.B_body_in_T[2] = magData->convertedZ;
-            rtU.B_meas_valid = magNormalOperation;
-            rtU.MT_on = 0;
+
+            /* get new magnetometer data from bdot magnetometer. */
+            read_magnetometer_data();
+
+            /* update data that will be fed into rt onestep */
+            update_simulink_info();
+
+            /* perform rt onestep after getting new bdot magnetometer readings */
             rt_OneStep();
-            updateMtqInfo();
-            sendTelemetry();
+
+            /* determine next mtq command to send out */
+            determine_mtq_commands();
+
+            /* send backchannel telemetry */
+            send_cosmos_telem();
+
+            /* update rollcall data */
             updateRCData();
-            update_rt_flag = 0;
-            count++;
+
+            /* Determine if mtq is ready to actuate */
+            if(mtq_state == MTQ_MEASUREMENT_PHASE)
+            {
+                send_dipole_packet(bdot_perspective_mtq_info.xDipole,
+                                   bdot_perspective_mtq_info.yDipole,
+                                   bdot_perspective_mtq_info.zDipole);
+                mtq_last_known_state.xDipole = bdot_perspective_mtq_info.xDipole;
+                mtq_last_known_state.yDipole = bdot_perspective_mtq_info.yDipole;
+                mtq_last_known_state.zDipole = bdot_perspective_mtq_info.zDipole;
+            }
+
+            /* clear rt_flag */
+            rt_flag = 0;
         }
 
-        if(count == 100)
-        {
-            send_all_polling_timers_segment();
-            count = 0;
-        }
-        if(send_dipole_flag && mtq_state == mag_valid)
-        {
-            sendDipolePacket(mtqInfo.xDipole, mtqInfo.yDipole, mtqInfo.zDipole);
-            lastKnownState.xDipole = mtqInfo.xDipole;
-            lastKnownState.yDipole = mtqInfo.yDipole;
-            lastKnownState.zDipole = mtqInfo.zDipole;
-            send_dipole_flag = 0;
-        }
-
+        /* update rollcall "aggressively" - David */
         rollcallUpdate();
-
     }
 
     bdot_controller_lib_terminate();
@@ -145,123 +159,111 @@ void initial_setup()
     P3OUT |= BIT5;
     P3OUT |= BIT4;
     P3DIR |= BIT4;
-    magNum = magInit(I2CBus1); // I2C bus 1
     P3DIR |= BIT5;
 
-    canWrapInit();
-    setCANPacketRxCallback(receive_packet);
+    /* initialize  bdot magnetometer i2c */
+    mag_num = magInit(I2CBus1); // I2C bus 1
 
+    /* initialize can */
+    canWrapInitWithFilter();
+    setCANPacketRxCallback(can_rx_callback);
+
+    /* temp */
     asensorInit(Ref_2p5V);
 
+    /* initialize rt onestep */
     bdot_controller_lib_initialize();
 
+    /* populate header for backchannel  */
     bcbinPopulateHeader(&hseg.header, TLM_ID_SHARED_HEALTH, sizeof(hseg));
     bcbinPopulateHeader(&myTelemMagnetometer.header, TLM_ID_MAGNETOMETER, sizeof(myTelemMagnetometer));
-    bcbinPopulateHeader(&myTelemMtqInfo.header, TLM_ID_MTQ_INFO, sizeof(myTelemMtqInfo));
+    bcbinPopulateHeader(&cosmos_bdot_persp_mtq_info.header, TLM_ID_MTQ_INFO, sizeof(cosmos_bdot_persp_mtq_info));
     bcbinPopulateHeader(&mySimulink.header, TLM_ID_SIMULINK_INFO, sizeof(mySimulink));
     bcbinPopulateHeader(&polling_timer_info.header, TLM_ID_POLLING_TIMER, sizeof(polling_timer_info));
     bcbinPopulateMeta(&metaSeg, sizeof(metaSeg));
+
+    /* TODO: Add comment */
     aggVec_init_i(&rc_temp);
     aggVec_init_i(&magX);
     aggVec_init_i(&magY);
     aggVec_init_i(&magZ);
+
+    /* initialize rollcall */
     rollcallInit(rollcallFunctions, sizeof(rollcallFunctions) / sizeof(rollcall_fn));
 
+    /* initialize timer */
     initializeTimer();
 }
 
-void getMagnetometerData()
+/* Update simulink information to send to rt onestep.
+ * B_body_inT[0] = magnetometer x
+ * B_body_inT[1] = magnetometer y
+ * B_body_inT[2] = magnetometer z
+ * Notice the order: x -> y -> z
+ * B_meas_valid = does not get processed
+ * MT_on = 0 always. Always send in the latest VALID magnetometer data*/
+void update_simulink_info()
 {
-    magData = testing_magReadXYZData(magNum, ConvertToTeslas);
+    rtU.B_body_in_T[0] = bdot_mag_data->convertedX;
+    rtU.B_body_in_T[1] = bdot_mag_data->convertedY;
+    rtU.B_body_in_T[2] = bdot_mag_data->convertedZ;
+    rtU.B_meas_valid = mag_norm_op;
+    rtU.MT_on = 0;
 }
 
-void performSelfTest()
+/* Read magnetometer data based on the current operation.
+ * For debugging purposes, use testing_magReadXYZData (for aardvark).
+ * Function to read magnetometer data returns a struct with xyz magnetometer and temp. */
+void read_magnetometer_data()
 {
-    selfTestConfig(magNum);
-    magNormalOperation = 0;
-}
-
-void performNormalOp()
-{
-    normalOperationConfig(magNum);
-    magNormalOperation = 1;
-}
-
-void simulink_compute()
-{
-    update_rt_flag = 1;
-    send_dipole_flag = 1;
-}
-
-
-void sendTelemetry()
-{
-    sendHealthSegment();
-    sendMagReadingSegment();
-    sendMtqInfoSegment();
-    sendSimulinkSegment();
-    bcbinSendPacket((uint8_t *) &metaSeg, sizeof(metaSeg));
-}
-
-void updateMtqInfo()
-{
-    mtqInfo.tumble_status  = (uint8_t) rtY.tumble;
-    if(mtqInfo.tumble_status)
+    switch(MAG_READING_CURR_OP)
     {
-        mtqInfo.xDipole = map((int8_t) rtY.Dig_val[0]);
-        mtqInfo.yDipole = map((int8_t) rtY.Dig_val[1]);
-        mtqInfo.zDipole = map((int8_t) rtY.Dig_val[2]);
-    } else
-    {
-        mtqInfo.xDipole = 0;
-        mtqInfo.yDipole = 0;
-        mtqInfo.zDipole = 0;
+        case MAG_READING_NORM_OP:
+            bdot_mag_data = magReadXYZData(mag_num, ConvertToTeslas);
+            break;
+        case MAG_READING_TEST_OP:
+            bdot_mag_data = testing_magReadXYZData(mag_num, ConvertToTeslas);
+            break;
+        default: magReadXYZData(mag_num, ConvertToTeslas);
     }
 }
 
-void sendHealthSegment()
+void determine_mtq_commands()
 {
-    // Populate and send the health segment for this entity
-    hseg.inttemp = asensorReadIntTempC();
-    hseg.oms = OMS_Unknown;
-    bcbinSendPacket((uint8_t *) &hseg, sizeof(hseg));
+    bdot_perspective_mtq_info.tumble_status  = (uint8_t) rtY.tumble;
+    if(bdot_perspective_mtq_info.tumble_status)
+    {
+        bdot_perspective_mtq_info.xDipole = map((int8_t) rtY.Dig_val[0]);
+        bdot_perspective_mtq_info.yDipole = map((int8_t) rtY.Dig_val[1]);
+        bdot_perspective_mtq_info.zDipole = map((int8_t) rtY.Dig_val[2]);
+    } else
+    {
+        bdot_perspective_mtq_info.xDipole = 0;
+        bdot_perspective_mtq_info.yDipole = 0;
+        bdot_perspective_mtq_info.zDipole = 0;
+    }
+}
 
-    // Also invoke the status handler for UART to send its health segments
-    debugInvokeStatusHandler(Entity_UART);
+/* Callback function tied to the one interrupt based timer. Sets flag at 10hz to
+ * call this function that sets flag for main loop to processed new magnetometer data and
+ * process data with rt onestep */
+void simulink_compute()
+{
+    rt_flag = 1;
+}
 
+/* send cosmos telemetry */
+void send_cosmos_telem()
+{
+    send_health_segment_cosmos();
+    send_bdot_mag_reading_cosmos();
+    send_mtq_info_segment_cosmos();
+    send_simulink_segment_cosmos();
+    bcbinSendPacket((uint8_t *) &metaSeg, sizeof(metaSeg));
 }
 
 
-void sendMagReadingSegment()
-{
-    myTelemMagnetometer.xMag = magData->convertedX * 1e9;
-    myTelemMagnetometer.yMag = magData->convertedY * 1e9;
-    myTelemMagnetometer.zMag = magData->convertedZ * 1e9;
-    myTelemMagnetometer.tempMag = magData->convertedTemp;
-
-    bcbinSendPacket((uint8_t *) &myTelemMagnetometer, sizeof(myTelemMagnetometer));
-}
-
-void sendMtqInfoSegment()
-{
-    myTelemMtqInfo.xDipole = lastKnownState.xDipole;
-    myTelemMtqInfo.yDipole = lastKnownState.yDipole;
-    myTelemMtqInfo.zDipole = lastKnownState.zDipole;
-
-    bcbinSendPacket((uint8_t *) &myTelemMtqInfo, sizeof(myTelemMtqInfo));
-}
-
-
-void sendSimulinkSegment()
-{
-    mySimulink.tumble = rtY.tumble;
-    mySimulink.sim_xDipole = rtY.Dig_val[0];
-    mySimulink.sim_yDipole = rtY.Dig_val[1];
-    mySimulink.sim_zDipole = rtY.Dig_val[2];
-    bcbinSendPacket((uint8_t *) &mySimulink, sizeof(mySimulink));
-}
-
-void sendDipolePacket(int8_t x, int8_t y, int8_t z)
+void send_dipole_packet(int8_t x, int8_t y, int8_t z)
 {
     CANPacket dipole_packet = {0};
     cmd_mtq_bdot dipole_info = {0};
@@ -274,63 +276,116 @@ void sendDipolePacket(int8_t x, int8_t y, int8_t z)
     canSendPacket(&dipole_packet);
 }
 
-void sendTumblePacket(uint8_t status)
+/* Switch magnetometer reading operation to do self test...lol */
+void perform_self_test()
 {
-    CANPacket tumble_packet = {0};
-    bdot_tumble_status tumble_info = {0};
-
-    tumble_info.bdot_tumble_status_status = status;
-
-    encodebdot_tumble_status(&tumble_info, &tumble_packet);
-    canSendPacket(&tumble_packet);
+    selfTestConfig(mag_num);
+    mag_norm_op = 0;
 }
 
+/* Switch magnetometer reading operation to do normal operation */
+void perform_norm_op()
+{
+    normalOperationConfig(mag_num);
+    mag_norm_op = 1;
+}
+
+/* convert val range from -127-127 to -100-100 */
 int map(int val)
 {
-    return mapGeneral(val, -127, 127, -100, 100);
+    return map_general(val, -127, 127, -100, 100);
 }
 
-int mapGeneral(int x, int in_min, int in_max, int out_min, int out_max)
+int map_general(int x, int in_min, int in_max, int out_min, int out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-/*No op function */
-void receive_packet(CANPacket *packet)
+/* Can packet receive callback function */
+void can_rx_callback(CANPacket *packet)
 {
-    if(packet->id == CAN_ID_MTQ_ACK)
+    mtq_ack ack = {0};
+    switch(packet->id)
     {
-        // measurement_phase = 0;
-        // actuation_phase = 1;
-        mtq_ack ack = {0};
-        decodemtq_ack(packet, &ack);
-        if(!ack.mtq_ack_phase)
-        {
-            mtq_state = MTQ_MEASUREMENT_PHASE;
-        } else
-        {
-            mtq_state = MTQ_ACTUATION_PHASE;
-        }
-    }
-    if(packet->id == CAN_ID_CMD_ROLLCALL)
-    {
-        rollcallStart();
+        case CAN_ID_MTQ_ACK:
+            // measurement_phase = 0;
+            // actuation_phase = 1;
+            decodemtq_ack(packet, &ack);
+            if(!ack.mtq_ack_phase)
+            {
+                mtq_state = MTQ_MEASUREMENT_PHASE;
+            } else
+            {
+                mtq_state = MTQ_ACTUATION_PHASE;
+            }
+            break;
+        case CAN_ID_CMD_ROLLCALL:
+            rollcallStart();
+            break;
+       // TODO: Add magnetometer packets from david to thu
     }
 }
 
+/* send health segment through backchannel */
+void send_health_segment_cosmos()
+{
+    // Populate and send the health segment for this entity
+    hseg.inttemp = asensorReadIntTempC();
+    hseg.oms = OMS_Unknown;
+    bcbinSendPacket((uint8_t *) &hseg, sizeof(hseg));
+
+    // Also invoke the status handler for UART to send its health segments
+    debugInvokeStatusHandler(Entity_UART);
+
+}
+
+/* send magnetometer reading segment through backchannel */
+void send_bdot_mag_reading_cosmos()
+{
+    myTelemMagnetometer.xMag = bdot_mag_data->convertedX * 1e9;
+    myTelemMagnetometer.yMag = bdot_mag_data->convertedY * 1e9;
+    myTelemMagnetometer.zMag = bdot_mag_data->convertedZ * 1e9;
+    myTelemMagnetometer.tempMag = bdot_mag_data->convertedTemp;
+
+    bcbinSendPacket((uint8_t *) &myTelemMagnetometer, sizeof(myTelemMagnetometer));
+}
+
+/* Send magnetorquer's state based on bdot's perspective */
+void send_mtq_info_segment_cosmos()
+{
+    cosmos_bdot_persp_mtq_info.xDipole = mtq_last_known_state.xDipole;
+    cosmos_bdot_persp_mtq_info.yDipole = mtq_last_known_state.yDipole;
+    cosmos_bdot_persp_mtq_info.zDipole = mtq_last_known_state.zDipole;
+
+    bcbinSendPacket((uint8_t *) &cosmos_bdot_persp_mtq_info, sizeof(cosmos_bdot_persp_mtq_info));
+}
+
+/* Send rt onestep output */
+void send_simulink_segment_cosmos()
+{
+    mySimulink.tumble = rtY.tumble;
+    mySimulink.sim_xDipole = rtY.Dig_val[0];
+    mySimulink.sim_yDipole = rtY.Dig_val[1];
+    mySimulink.sim_zDipole = rtY.Dig_val[2];
+    bcbinSendPacket((uint8_t *) &mySimulink, sizeof(mySimulink));
+}
+
+
+/* debugging purpose function */
 void send_all_polling_timers_segment()
 {
     user_timer_polling_info polling_info[NUM_SUPPORTED_DURATIONS_POLLING];
-    get_polling_timer_info(polling_info);
+    debug_polling_timer_info(polling_info);
     bcbinSendPacket((uint8_t *) &polling_timer_info, sizeof(polling_timer_info));
 }
 
 
+/* update rollcall packet */
 void updateRCData()
 {
-    aggVec_push_i(&magX, magData->rawX);
-    aggVec_push_i(&magY, magData->rawY);
-    aggVec_push_i(&magZ, magData->rawZ);
+    aggVec_push_i(&magX, bdot_mag_data->rawX);
+    aggVec_push_i(&magY, bdot_mag_data->rawY);
+    aggVec_push_i(&magZ, bdot_mag_data->rawZ);
     aggVec_push_i(&rc_temp, asensorReadIntTempRawC());
 }
 
@@ -378,6 +433,51 @@ void rt_OneStep(void)
 }
 
 
+void rcPopulate1(CANPacket *out)
+{
+    rc_adcs_bdot_1 rc;
+    rc.rc_adcs_bdot_1_reset_count = bspGetResetCount();
+    rc.rc_adcs_bdot_1_sysrstiv = SYSRSTIV;
+    rc.rc_adcs_bdot_1_temp_avg = aggVec_avg_i_i(&rc_temp);
+    rc.rc_adcs_bdot_1_temp_max = aggVec_max_i(&rc_temp);
+    rc.rc_adcs_bdot_1_temp_min = aggVec_min_i(&rc_temp);
+    rc.rc_adcs_bdot_1_reset_count = 0;
+    aggVec_reset((aggVec *)&rc_temp);
+    encoderc_adcs_bdot_1(&rc, out);
+}
+
+void rcPopulate2(CANPacket *out)
+{
+    rc_adcs_bdot_2 rc ;
+    rc.rc_adcs_bdot_2_mag_x_min = aggVec_min_i(&magX);
+    rc.rc_adcs_bdot_2_mag_x_max = aggVec_max_i(&magX);
+    rc.rc_adcs_bdot_2_mag_x_avg = aggVec_avg_i_i(&magX);
+    rc.rc_adcs_bdot_2_mag_y_min = aggVec_min_i(&magY);
+    aggVec_reset((aggVec *)&magX);
+    encoderc_adcs_bdot_2(&rc, out);
+}
+
+void rcPopulate3(CANPacket *out)
+{
+    rc_adcs_bdot_3 rc = {0};
+    rc.rc_adcs_bdot_3_mag_y_max = aggVec_max_i(&magY);
+    rc.rc_adcs_bdot_3_mag_y_avg = aggVec_avg_i_i(&magY);
+    rc.rc_adcs_bdot_3_mag_z_min = aggVec_min_i(&magZ);
+    rc.rc_adcs_bdot_3_mag_z_max = aggVec_max_i(&magZ);
+    aggVec_reset((aggVec *)&magY);
+    encoderc_adcs_bdot_3(&rc, out);
+}
+
+
+void rcPopulate4(CANPacket *out)
+{
+    rc_adcs_bdot_4 rc = {0};
+    rc.rc_adcs_bdot_4_mag_z_avg = aggVec_avg_i_i(&magZ);
+    rc.rc_adcs_bdot_4_tumble = rtY.tumble;
+    aggVec_reset((aggVec *)&magZ);
+    encoderc_adcs_bdot_4(&rc, out);
+}
+
 // Will be called when PPT firing cycle is starting (sent via CAN by the PPT)
 void handlePPTFiringNotification()
 {
@@ -391,52 +491,3 @@ void handleRollCall()
 {
     __no_operation();
 }
-
-void rcPopulate1(CANPacket *out)
-{
-    rc_adcs_bdot_1 rc;
-    rc.rc_adcs_bdot_1_reset_count = bspGetResetCount();
-    rc.rc_adcs_bdot_1_sysrstiv = SYSRSTIV;
-    rc.rc_adcs_bdot_1_temp_avg = aggVec_avg_i_i(&rc_temp);
-    rc.rc_adcs_bdot_1_temp_max = aggVec_max_i(&rc_temp);
-    rc.rc_adcs_bdot_1_temp_min = aggVec_min_i(&rc_temp);
-    rc.rc_adcs_bdot_1_reset_count = 0;
-    aggVec_reset((aggVec *)&rc_temp);
-    encoderc_adcs_bdot_1(&rc, out);
-}
-void rcPopulate2(CANPacket *out)
-{
-    rc_adcs_bdot_2 rc ;
-    rc.rc_adcs_bdot_2_mag_x_min = aggVec_min_i(&magX);
-    rc.rc_adcs_bdot_2_mag_x_max = aggVec_max_i(&magX);
-    rc.rc_adcs_bdot_2_mag_x_avg = aggVec_avg_i_i(&magX);
-    rc.rc_adcs_bdot_2_mag_y_min = aggVec_min_i(&magY);
-    aggVec_reset((aggVec *)&magX);
-    encoderc_adcs_bdot_2(&rc, out);
-}
-void rcPopulate3(CANPacket *out)
-{
-    rc_adcs_bdot_3 rc = {0};
-    rc.rc_adcs_bdot_3_mag_y_max = aggVec_max_i(&magY);
-    rc.rc_adcs_bdot_3_mag_y_avg = aggVec_avg_i_i(&magY);
-    rc.rc_adcs_bdot_3_mag_z_min = aggVec_min_i(&magZ);
-    rc.rc_adcs_bdot_3_mag_z_max = aggVec_max_i(&magZ);
-    aggVec_reset((aggVec *)&magY);
-    encoderc_adcs_bdot_3(&rc, out);
-}
-void rcPopulate4(CANPacket *out)
-{
-    rc_adcs_bdot_4 rc = {0};
-    rc.rc_adcs_bdot_4_mag_z_avg = aggVec_avg_i_i(&magZ);
-    rc.rc_adcs_bdot_4_tumble = rtY.tumble;
-    aggVec_reset((aggVec *)&magZ);
-    encoderc_adcs_bdot_4(&rc, out);
-}
-
-//void start_telem_timer()
-//{
-//    telem_timer = timerPollInitializer(telem_timer_ms);
-//}
-
-//FILE_STATIC int telem_timer;
-//FILE_STATIC uint16_t telem_timer_ms = 200; // 1 s
