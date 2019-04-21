f0ed404a99595fd2598e17526557c95394f3c040
Eli Reed <sailedeer11@gmail.com>
2019-04-21 14:21:38 -0700

    some error handling for i2c operations and updated altitude calculation

--- src/dsbase/sensors/altimeter.c
+++ src/dsbase/sensors/altimeter.c
@@ -44,12 +44,12 @@ void baromInit(bus_instance_i2c bus)
  * Reads and returns a single 16-bit calibration coefficient from the sensor's PROM.
  * int reg: offset from start address
  */
-uint16_t readPROMWord(int8_t reg)
+static uint16_t readPROMWord(int8_t reg)
 {
     uint16_t word = 0;
     reg = reg * 2;
     uint8_t addr = MS5611_PROM_START_ADDR + reg;
-    i2cMasterRegisterRead(hBarom, addr, i2cBuff, 2);
+    i2c_result result = i2cMasterRegisterRead(hBarom, addr, i2cBuff, 2);
     word = (uint16_t)(i2cBuff[1]) | ((uint16_t)(i2cBuff[0]) << 8);   // MSB first
     return word;
 }
@@ -57,10 +57,10 @@ uint16_t readPROMWord(int8_t reg)
 /*
  * Send sensor reset command.
  */
-void reset()
+static void reset()
 {
     i2cBuff[0] = MS5611_RESET;
-    i2cMasterWrite(hBarom, i2cBuff, 1);
+    i2c_result result = i2cMasterWrite(hBarom, i2cBuff, 1);
     wait(3);
 }
 
@@ -71,26 +71,29 @@ void reset()
 static void send_conversion(uint8_t cmd)
 {
     i2cBuff[0] = MS5611_CONV + cmd;
-    i2cMasterWrite(hBarom, i2cBuff, 1);
-    switch (cmd & 0x0F)                     // wait necessary amount of time
+    i2c_result result = i2cMasterWrite(hBarom, i2cBuff, 1);
+    if (result == i2cRes_noerror)
     {
-        case MS5611_CONV_OSR256:
-            wait(1);
-            break;
-        case MS5611_CONV_OSR512:
-            wait(3);
-            break;
-        case MS5611_CONV_OSR1024:
-            wait(4);
-            break;
-        case MS5611_CONV_OSR2048:
-            wait(6);
-            break;
-        case MS5611_CONV_OSR4096:
-            wait(10);
-            break;
+        switch (cmd & 0x0F)                     // wait necessary amount of time
+        {
+            case MS5611_CONV_OSR256:
+                wait(1);
+                break;
+            case MS5611_CONV_OSR512:
+                wait(3);
+                break;
+            case MS5611_CONV_OSR1024:
+                wait(4);
+                break;
+            case MS5611_CONV_OSR2048:
+                wait(6);
+                break;
+            case MS5611_CONV_OSR4096:
+                wait(10);
+                break;
+        }
+        return;
     }
-    return;
 }
 
 /*
@@ -102,45 +105,49 @@ static void send_conversion(uint8_t cmd)
 static float read_adc(uint8_t conv_cmd, uint8_t osr)
 {
     send_conversion(conv_cmd + osr);
-    i2cMasterRegisterRead(hBarom, MS5611_ADC_ADDR, i2cBuff, 3);
-    uint32_t rec = ((uint32_t)(i2cBuff[2]) | ((uint32_t)(i2cBuff[1]) << 8)) | ((uint32_t)(i2cBuff[0]) << 16);
-
-    int32_t ret;
-    switch (conv_cmd)
+    i2c_result result = i2cMasterRegisterRead(hBarom, MS5611_ADC_ADDR, i2cBuff, 3);
+    if (result == i2cRes_noerror)
     {
-        case MS5611_D1_CONV:
+        uint32_t rec = ((uint32_t)(i2cBuff[2]) | ((uint32_t)(i2cBuff[1]) << 8)) | ((uint32_t)(i2cBuff[0]) << 16);
+
+        int32_t ret;
+        switch (conv_cmd)
         {
-            int32_t tDiff2 = 0;
-            int64_t off2 = 0;
-            int64_t sens2 = 0;
-
-            if (tActual < 2000)
+            case MS5611_D1_CONV:
             {
-                tDiff2 = pow(tDiff, 2) / pow(2, 31);
-                off2 = 5 * pow((tActual - 2000), 2) / 2;
-                sens2 = 5 * pow((tActual - 2000), 2) / 4;
-                if (tActual < -1500)
-                {
-                    off2 = off2 + 7 * pow((tActual + 1500), 2);
-                    sens2 = sens2 + (11 * pow((tActual + 1500), 2) / 2);
-                }
-            }
+                int32_t tDiff2 = 0;
+                int64_t off2 = 0;
+                int64_t sens2 = 0;
 
-            int64_t pOff = prom[C2] * pow(2, 17) + prom[C4] * tDiff / pow(2, 6);
-            int64_t pSens = prom[C1] * pow(2, 16) + prom[C3] * tDiff / pow(2, 7);
-            ret = (rec * pSens / pow(2, 21) - pOff) / pow(2, 16);
-            break;
+                if (tActual < 2000)
+                {
+                    tDiff2 = pow(tDiff, 2) / pow(2, 31);
+                    off2 = 5 * pow((tActual - 2000), 2) / 2;
+                    sens2 = 5 * pow((tActual - 2000), 2) / 4;
+                    if (tActual < -1500)
+                    {
+                        off2 = off2 + 7 * pow((tActual + 1500), 2);
+                        sens2 = sens2 + (11 * pow((tActual + 1500), 2) / 2);
+                    }
+                }
+
+                int64_t pOff = prom[C2] * pow(2, 17) + prom[C4] * tDiff / pow(2, 6);
+                int64_t pSens = prom[C1] * pow(2, 16) + prom[C3] * tDiff / pow(2, 7);
+                ret = (rec * pSens / pow(2, 21) - pOff) / pow(2, 16);
+                break;
+            }
+            case MS5611_D2_CONV:
+                tDiff = rec - prom[C5] * pow(2, 8);
+                ret = 2000 + (tDiff * prom[C6]) / pow(2, 23);
+                tActual = ret;
+                break;
         }
-        case MS5611_D2_CONV:
-            tDiff = rec - prom[C5] * pow(2, 8);
-            ret = 2000 + (tDiff * prom[C6]) / pow(2, 23);
-            tActual = ret;
-            break;
+        return (float)ret / 100;
     }
-    return (float)ret / 100;
+    return -1;
 }
 
-float calculateAltitude()
+static float calculateAltitude()
 {
     float staticPressure;
     float standardTemp;
@@ -177,8 +184,9 @@ float calculateAltitude()
         layerHeight = 0;
     }
 
-    float exponent =  (GAS_CONSTANT * lapseRate) / (G_0 * AIR_MOLAR_MASS);
-    altitude = (standardTemp / lapseRate) * (1 - pow((altData.pressure / staticPressure), exponent)) + layerHeight;
+    // need to add eq 2 for when lapse rate is equal to 0
+    float exponent = (GAS_CONSTANT * lapseRate) / (G_0 * AIR_MOLAR_MASS);
+    altitude = layerHeight + (standardTemp / lapseRate)(pow((staticPressure / altData.pressure), exponent) - 1);
     lastAltitude = altitude;
     return altitude;
 }
